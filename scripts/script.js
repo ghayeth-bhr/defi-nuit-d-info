7087d15a0cd5c126d514dc8e677c47b7a11ae109
ghayeth-bhr <ghayeth-bhr@localhost.localdomain>
2024-12-08 15:31:26 +0100

    Initial commit with project files

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Nuit de l'info 2023 - Équipe : PolyCartel - Établissement : Polytech Tours
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Code exécuté au chargement de la page
+});
